763e068fde773d0a868cd81056413ed90dda0137
Danny JeanLouis <dlouis3386@gmail.com>
2021-04-20 03:04:35 -0400

    fixed open menu scrolling issue

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,14 @@
+const toggleMenu = document.querySelector("#toggleMenu");
+const toggleClose = document.querySelector("#close");
 
+toggleMenu.addEventListener("click", (evt) => {
+  if (evt.target.checked) {
+    document.body.style.overflow = "hidden";
+  }
+});
+
+toggleClose.addEventListener("click", (evt) => {
+  if (evt.target.checked) {
+    document.body.style.overflow = "scroll";
+  }
+});
